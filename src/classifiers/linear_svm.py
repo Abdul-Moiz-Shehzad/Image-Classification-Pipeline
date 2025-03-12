7f8636cf6805635775f209db31d5202dee68115d
Abdul Moiz <abdulmoizfort@gmail.com>
2025-03-12 11:33:52 +0500

    SVM Classifier

---
+++ src/classifiers/linear_svm.py
@@ -0,0 +1,113 @@
+import torch
+
+
+def svm_loss_naive(W, X, y, reg):
+    """
+    Structured SVM loss function, naive implementation (with loops).
+
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
+
+    Inputs:
+    - W: A PyTorch tensor of shape (D, C) containing weights.
+    - X: A PyTorch tensor of shape (N, D) containing a minibatch of data.
+    - y: A PyTorch tensor of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
+
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    dW = torch.zeros_like(W)  # initialize the gradient as zero
+
+    # compute the loss and the gradient
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    loss = 0.0
+    for i in range(num_train):
+        scores = X[i] @ W
+        correct_class_score = scores[y[i]]
+        for j in range(num_classes):
+            if j == y[i]:
+                continue
+            margin = scores[j] - correct_class_score + 1  # note delta = 1
+            if margin > 0:
+                loss += margin
+
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+
+    # Add regularization to the loss.
+    loss += reg * torch.sum(W * W)
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Compute the gradient of the loss function and store it dW.                #
+    # Rather than first computing the loss and then computing the derivative,   #
+    # it may be simpler to compute the derivative at the same time that the      #
+    # loss is being computed. As a result you may need to modify some of the     #
+    # code above to compute the gradient.                                       #
+    #############################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    for i in range(num_train):
+        scores = X[i] @ W  # (C,)
+        correct_class_score = scores[y[i]]
+        for j in range(num_classes):
+            if j == y[i]:
+                continue
+            margin = scores[j] - correct_class_score + 1
+            if margin > 0:
+                dW[:, j] += X[i]
+                dW[:, y[i]] -= X[i]
+    dW /= num_train
+    dW += 2 * reg * W
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    return loss, dW
+
+
+def svm_loss_vectorized(W, X, y, reg):
+    """
+    Structured SVM loss function, vectorized implementation.
+
+    Inputs and outputs are the same as svm_loss_naive.
+    """
+    loss = 0.0
+    dW = torch.zeros_like(W)  # initialize the gradient as zero
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the structured SVM loss, storing the    #
+    # result in loss.                                                           #
+    #############################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    scores = X @ W  # (N, C)
+    correct_class_scores = scores[torch.arange(X.shape[0]), y].unsqueeze(1)  # (N, 1)
+    margins = scores - correct_class_scores + 1  # (N, C)
+    margins[torch.arange(X.shape[0]), y] = 0  # zero out correct class margins
+    margins = torch.clamp(margins, min=0)
+    loss = torch.sum(margins) / X.shape[0]
+    loss += reg * torch.sum(W * W)
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the gradient for the structured SVM     #
+    # loss, storing the result in dW.                                           #
+    #                                                                           #
+    # Hint: Instead of computing the gradient from scratch, it may be easier    #
+    # to reuse some of the intermediate values that you used to compute the     #
+    # loss.                                                                     #
+    #############################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    mask = (margins > 0).float()  # (N, C)
+    row_sum = torch.sum(mask, dim=1)  # (N,)
+    mask[torch.arange(X.shape[0]), y] = -row_sum
+    dW = X.t() @ mask  # (D, C)
+    dW /= X.shape[0]
+    dW += 2 * reg * W
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    return loss, dW
